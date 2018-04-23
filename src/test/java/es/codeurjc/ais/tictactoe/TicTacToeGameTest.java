324f9c9bc1662049c2511c87abf80f78138eb3a0
jlojosnegros <jl.ojosnegros.manchon@gmail.com>
2018-04-23 22:29:22 +0200

    Class for test anything regarding TTTgame that does not need parameters

--- src/test/java/es/codeurjc/ais/tictactoe/TicTacToeGameTest.java
+++ src/test/java/es/codeurjc/ais/tictactoe/TicTacToeGameTest.java
@@ -1,11 +1,7 @@
 package es.codeurjc.ais.tictactoe;
 
+import org.junit.Before;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.reset;
@@ -13,22 +9,27 @@ import static org.mockito.Mockito.verify;
 import static org.hamcrest.CoreMatchers.hasItems;
 import static org.mockito.hamcrest.MockitoHamcrest.argThat;
 
-
 public class TicTacToeGameTest {
 
-    @Test
-    public void test() {
+    private TicTacToeGame game;
+    private Player playerOne;
+    private Player playerTwo;
+    private Connection connectionPlayerOne;
+    private Connection connectionPlayerTwo;
 
-        TicTacToeGame game = new TicTacToeGame();
+    @Before
+    public void BeforeEach() {
 
-        Connection connectionPlayerOne= mock(Connection.class);
-        Connection connectionPlayerTwo= mock(Connection.class);
+        this.game = new TicTacToeGame();
+
+        this.playerOne = new Player(1, "X", "player_one");
+        this.playerTwo = new Player(2, "O", "player_two");
+
+        this.connectionPlayerOne = mock(Connection.class);
+        this.connectionPlayerTwo = mock(Connection.class);
 
         game.addConnection(connectionPlayerOne);
         game.addConnection(connectionPlayerTwo);
-
-        Player playerOne = new Player(1, "X", "player_one");
-        Player playerTwo = new Player(2, "O", "player_two");
 
         game.addPlayer(playerOne);
 
@@ -36,25 +37,26 @@ public class TicTacToeGameTest {
         reset(connectionPlayerTwo);
 
         game.addPlayer(playerTwo);
+    }
 
-        verify(connectionPlayerOne).sendEvent(eq(TicTacToeGame.EventType.JOIN_GAME), argThat(hasItems(playerOne,playerTwo)));
-        verify(connectionPlayerTwo).sendEvent(eq(TicTacToeGame.EventType.JOIN_GAME), argThat(hasItems(playerOne,playerTwo)));
-
-
+    @Test
+    public void testJoinGameEvent() {
+        verify(connectionPlayerOne).sendEvent(eq(TicTacToeGame.EventType.JOIN_GAME), argThat(hasItems(playerOne, playerTwo)));
+        verify(connectionPlayerTwo).sendEvent(eq(TicTacToeGame.EventType.JOIN_GAME), argThat(hasItems(playerOne, playerTwo)));
+    }
         /// play
 
-        game.mark(0); // X
-        game.mark(3); // O
-        game.mark(1); // X
-        game.mark(4); // O
-        game.mark(2); // X -- win
+//        game.mark(0); // X
+//        game.mark(3); // O
+//        game.mark(1); // X
+//        game.mark(4); // O
+//        game.mark(2); // X -- win
+//
+//        ArgumentCaptor<TicTacToeGame.WinnerValue> argument = ArgumentCaptor.forClass(TicTacToeGame.WinnerValue.class);
+//        verify(connectionPlayerOne).sendEvent(eq(TicTacToeGame.EventType.GAME_OVER), argument.capture());
+//        TicTacToeGame.WinnerValue event = argument.getValue();
+//
+//        assertThat(event, is(notNullValue()) );
+//        assertThat(event.player, is(playerOne));
 
-        ArgumentCaptor<TicTacToeGame.WinnerValue> argument = ArgumentCaptor.forClass(TicTacToeGame.WinnerValue.class);
-        verify(connectionPlayerOne).sendEvent(eq(TicTacToeGame.EventType.GAME_OVER), argument.capture());
-        TicTacToeGame.WinnerValue event = argument.getValue();
-
-        assertThat(event, is(notNullValue()) );
-        assertThat(event.player, is(playerOne));
-
-    }
 }
