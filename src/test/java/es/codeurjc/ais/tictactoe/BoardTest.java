7f3bd3aa18e357e2ba9e2725e42e8cc3b3ae563f
jlojosnegros <jl.ojosnegros.manchon@gmail.com>
2018-04-23 20:35:48 +0200

    Make a parameterized test for Board.
    CAUTION!!! there is an error on Board::checkDraw method!!!

--- src/test/java/es/codeurjc/ais/tictactoe/BoardTest.java
+++ src/test/java/es/codeurjc/ais/tictactoe/BoardTest.java
@@ -3,18 +3,49 @@ package es.codeurjc.ais.tictactoe;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runners.Parameterized.Parameter;
 
-import static junit.framework.TestCase.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collection;
+
 import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.assertNull;
+import static junit.framework.TestCase.assertTrue;
+import static junit.framework.TestCase.assertFalse;
 
 
+@RunWith(Parameterized.class)
 public class BoardTest {
+
+    private static int WIN_X= 0;
+    private static int WIN_O= 1;
+    private static int DRAW = 2;
+
+    @Parameters
+    public static Collection<Object[]> data () {
+        Object[][] values = {
+                { new int[] { 0,3,1,4,2}, WIN_X },
+                { new int[] { 0,3,1,4,8,5}, WIN_O },
+                { new int[] { 0,3,1,4,7,6,5,2,8}, DRAW },
+        };
+
+        return Arrays.asList(values);
+    }
+
+    @Parameter(0) public int[] moves;
+    @Parameter(1) public int operation;
+
+
 
     private Board board;
     private static String[] labels;
 
     @BeforeClass
-    public  void BeforeAll() {
+    static public  void BeforeAll() {
         labels = new String[2];
         labels[0] = "X";
         labels[1] = "O";
@@ -26,17 +57,33 @@ public class BoardTest {
     }
 
     @Test
-    public void testSimpleWin() {
+    public void testSimpleGames() {
 
-        int[] moves = { 0, 1, 2};
+        // given an empty board -- see BeforeEach
 
-        //for (int[] winPos : winPositions)
+        //when I play the given moves ...
+        int index = 0;
+        for (int move : moves) {
 
-        for(int move : moves) {
             TicTacToeGame.Cell cell = board.getCell(move);
-            cell.value = "X";
+            cell.value = labels[index];
+            index = (index + 1) % (labels.length);
         }
 
-        assertNotNull(board.getCellsIfWinner("X"));
+
+        //then one player should win or there should be draw
+        if (operation == WIN_X) {
+            assertNotNull(board.getCellsIfWinner("X"));
+            assertNull(board.getCellsIfWinner("O"));
+            assertFalse(board.checkDraw());
+        } else if (operation == WIN_O) {
+            assertNull(board.getCellsIfWinner("X"));
+            assertNotNull(board.getCellsIfWinner("O"));
+            assertFalse(board.checkDraw());
+        } else {
+            assertTrue(board.checkDraw());  // ERROR!!!!! There is no draw!!!
+            assertNull(board.getCellsIfWinner("X"));
+            assertNull(board.getCellsIfWinner("O"));
+        }
     }
 }
