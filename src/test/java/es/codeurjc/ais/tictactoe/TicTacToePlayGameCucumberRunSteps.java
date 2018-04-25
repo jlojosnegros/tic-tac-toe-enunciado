8111a359e042e9767d07f80cab7ce4b0ce6d64b9
jlojosnegros <jl.ojosnegros.manchon@gmail.com>
2018-04-25 11:39:10 +0200

    Before and After each cucumber test

--- src/test/java/es/codeurjc/ais/tictactoe/TicTacToePlayGameCucumberRunSteps.java
+++ src/test/java/es/codeurjc/ais/tictactoe/TicTacToePlayGameCucumberRunSteps.java
@@ -3,13 +3,32 @@ package es.codeurjc.ais.tictactoe;
 
 import cucumber.api.PendingException;
 import cucumber.api.Transform;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import cucumber.api.java.en.And;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 public class TicTacToePlayGameCucumberRunSteps {
 
+    WebDriver driverPlayerOne;
+    WebDriver driverPlayerTwo;
+    @Before
+    public void beforeEach() {
+        System.out.println("BEFORE_EACH");
+        driverPlayerOne = new ChromeDriver();
+        driverPlayerTwo = new ChromeDriver();
+    }
+
+    @After
+    public void afterEach() {
+        System.out.println("AFTER_EACH");
+        releaseWebDriver(driverPlayerOne);
+        releaseWebDriver(driverPlayerTwo);
+    }
     @Given("^I have a tictactoe game at (-?.*)$")
     public void i_have_a_tictactoe_game_at(String url) throws Throwable {
     }
